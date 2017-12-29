9f504b34e8c66e5adcb3b36c1ff770c64e6bf95c
Võ Duy Thảo <duythao56@gmail.com>
2017-12-29 20:41:55 +0700

    fix order detail dao

--- JDS-ManagementApp/src/main/java/Model/DAO/OrderDetailDAO.java
+++ JDS-ManagementApp/src/main/java/Model/DAO/OrderDetailDAO.java
@@ -4,14 +4,17 @@ import Model.DTO.OrderDetail;
 import Model.DTO.OrderDetailId;
 
 public interface OrderDetailDAO {
+	
+	public Iterable<OrderDetail> findAll();
+	
+	public OrderDetail save(OrderDetail orderDetail);
 
-public OrderDetail save(OrderDetail orderDetail);
-	
 	public OrderDetail update(OrderDetail orderDetail);
-	
+
 	public void delete(OrderDetailId id);
 	
 	public OrderDetail findById(OrderDetailId id);
 	
-	public Iterable<OrderDetail> findAll();
+	public Iterable<OrderDetail> findByOrderId(int orderId);
+	
 }
