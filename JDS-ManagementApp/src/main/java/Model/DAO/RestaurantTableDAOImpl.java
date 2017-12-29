f753b62239377b730d6ed312a6ff9bd3e17a9766
Võ Duy Thảo <duythao56@gmail.com>
2017-12-29 20:53:18 +0700

    do pagination, count for restauranttableDAO

--- JDS-ManagementApp/src/main/java/Model/DAO/RestaurantTableDAOImpl.java
+++ JDS-ManagementApp/src/main/java/Model/DAO/RestaurantTableDAOImpl.java
@@ -1,11 +1,25 @@
 package Model.DAO;
 
+import java.util.Date;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import Model.DTO.Food;
 import Model.DTO.RestaurantTable;
+import Model.MODEL.Page;
+import Model.MODEL.PageQuery;
 
+@Transactional
 public class RestaurantTableDAOImpl implements RestaurantTableDAO {
 
 	@Autowired
@@ -20,6 +34,8 @@ public class RestaurantTableDAOImpl implements RestaurantTableDAO {
 	public RestaurantTable save(RestaurantTable restaurantTable) {
 		// TODO Auto-generated method stub
 		Session session = this.sessionFactory.getCurrentSession();
+		restaurantTable.setDateCreated(new Date());
+		restaurantTable.setFlags(false);
 		session.persist(restaurantTable);
 		return restaurantTable;
 	}
@@ -57,5 +73,37 @@ public class RestaurantTableDAOImpl implements RestaurantTableDAO {
 		Session session = this.sessionFactory.getCurrentSession();
 		Iterable<RestaurantTable> list = session.createQuery("from RestaurantTable").list();
 		return list;
+	}
+
+	@Override
+	public Page paginateRestaurantTable(PageQuery pageQuery, int departmentId) {
+		// TODO Auto-generated method stub
+		System.out.println("zo : " + departmentId );
+		int start = (pageQuery.getPage() - 1) * pageQuery.getSize();
+		long count = 0;
+		long totalPages = 0;
+		System.out.println("zo3 : " + departmentId );
+		Session session = this.sessionFactory.getCurrentSession();
+
+		
+		Criteria criteria = session.createCriteria(RestaurantTable.class).add(Restrictions.eq("departmentId", departmentId));
+		criteria.setFirstResult(start);
+		criteria.setMaxResults(pageQuery.getSize());
+		criteria.addOrder(pageQuery.isAsc() ? Order.asc(pageQuery.getSortBy()) : Order.desc(pageQuery.getSortBy()));
+		
+		System.out.println("zo2 : "  );
+		if(pageQuery.getSearchBy() != null && pageQuery.getSearchText() != null) {
+			System.out.println(pageQuery.getSearchText() +  pageQuery.getSearchBy());
+			Criterion criterion = Restrictions.like(pageQuery.getSearchBy(), pageQuery.getSearchText(), MatchMode.ANYWHERE);
+			criteria.add(criterion);
+		}
+		
+		Iterable<RestaurantTable> list = criteria.list();
+		count = (long) criteria.setProjection(Projections.rowCount()).uniqueResult();
+		totalPages = (count % pageQuery.getSize() != 0) ? (count/pageQuery.getSize()) + 1 : count/pageQuery.getSize();
+		Page page = new Page(list , totalPages);
+		System.out.println("count : " + count );
+		System.out.println("page : "  + page.getContent());
+		return page;
 	}
 }
